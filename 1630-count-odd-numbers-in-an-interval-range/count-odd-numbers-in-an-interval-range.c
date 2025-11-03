31976c4c5f7f1594781b580f36d169ec8c9242bd
leavin1611 <leavin1611@gmail.com>
2025-11-03 14:45:03 +0530

    Time: 1724 ms (34.05%) | Memory: 7.8 MB (22.76%) - LeetSync

---
+++ 1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.c
@@ -0,0 +1,13 @@
+
+int countOdds(int low, int high){
+    int count=0;
+    int k=low,n=high;
+    for(int i=k;i<=n;i++)
+    {
+        if(i%2!=0)
+        {
+            count++;
+        }
+    }
+return count;
+}
